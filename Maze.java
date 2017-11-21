9f7e966da0e27b9046d5f45e8ea233084b51fbf3
Ray-Luo <leiluo.ray@gmail.com>
2017-11-20 22:11:12 -0600

    finished Maze

--- Maze.java
+++ Maze.java
@@ -1,101 +1,96 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Maze{
-    public static void Maze(String[] args){
-        
+    public static void main(String[] args){
+        char[][] maze = {
+		{'.','.','X','.','.'},
+		{'X','.','.','X','X'},
+		{'.','X','.','.','.'},
+		{'.','X','X','.','.'},
+		{'.','X','.','X','.'}
+	};
+
+	//if(solve1(maze, 0, 0, 4, 4)){
+	//	System.out.println("solve1 Good");
+	//}
+
+	//if(solve2(maze, 0, 0, 4, 4)){
+	//	System.out.println("solve2 Good");
+	//}
+	
+	List<Character> path = new ArrayList<>();
+	if(solve3(maze, 0, 0, 4, 4, path)){
+		System.out.println("solve3 Good");
+		System.out.println(path.toString());
+	}
+
+
     }
 
-    public static boolean solveMaze(char[][] maze, int startX, int StartY, int targetX, int targetY, boolean[][] visited){
-        if(startX < 0 || startX >= maze.length || startY < 0 || startY >= maze[0].length || maze[startX][startY] == 'X' || visited[startx][startY]){
+    public static boolean solve1(char[][] maze, int startX, int startY, int targetX, int targetY){
+    
+	if(startX == targetX && startY == targetY){
+		return true;
+	}
+
+	if(startX >= maze.length || startY >= maze[0].length || startX < 0 || startY < 0 || maze[startX][startY] == 'X'){
 		return false;
 	}
 
-	if(startX == targetX && startY == targetY){
-	    return true;
-	}
-
-	visited[startX][startY] = true;
-
-	if(solveMaze(maze, startX + 1, startY, targetX, targetY, visited) || solveMaze(maze, startX - 1, startY, targetX, targetY, visited) || solveMaze(maze, startX, startY + 1, targetX, targetY, visited) || solveMaze(maze, startX, startY - 1, targetX, targetY, visited)){
-	    return true;
-	}
-
-	return false;
-    }
-
-
-    public static boolean solveMaze2(char[][] maze, int startX, int StartY, int targetX, int targetY){
-        if(startX < 0 || startX >= maze.length || startY < 0 || startY >= maze[0].length || maze[startX][startY] == 'X' || visited[startx][startY]){
-		return false;
-	}
-
-	if(startX == targetX && startY == targetY){
-	    return true;
-	}
-
+	
 	maze[startX][startY] = 'X';
 
-	int[] dx = {1, 0, -1, 0};
-	int[] dy = {0, 1, 0, -1};
+	return solve1(maze, startX + 1, startY, targetX, targetY) || solve1(maze, startX - 1, startY, targetX, targetY) || solve1(maze, startX, startY + 1, targetX, targetY) || solve1(maze, startX, startY - 1, targetX, targetY);
 
-	for(int i = 0; i < 4; i++){
-	    if(solveMaze2(maze, startX + dx[i], startY + dy[i], targetX, targetY)){
-	        return true;
-	    }
-	}
-	return false;
     }
 
-    public static boolean printMaze(char[][] maze, int startX, int StartY, int targetX, int targetY, String path){
-        if(startX < 0 || startX >= maze.length || startY < 0 || startY >= maze[0].length || maze[startX][startY] == 'X' || visited[startx][startY]){
+    public static boolean solve2(char[][] maze, int startX, int startY, int targetX, int targetY){
+    	
+	if(startX >= maze.length || startY >= maze[0].length || startX < 0 || startY < 0 || maze[startX][startY] == 'X'){
 		return false;
 	}
 
 	if(startX == targetX && startY == targetY){
-	    System.out.println(path);
-	    return true;
+		return true;
 	}
 
+	
 	maze[startX][startY] = 'X';
-
-	int[] dx = {1, 0, -1, 0};
-	int[] dy = {0, 1, 0, -1};
-        char[] direction = {'D', 'R', 'U', 'L'};
+   	int[] xMove = {1, 0, -1, 0};
+	int[] yMove = {0, 1, 0, -1};
 
 	for(int i = 0; i < 4; i++){
-	    String newPath = path + direction[i] + " ";
-	    if(printMaze(maze, startX + dx[i], startY + dy[i], targetX, targetY)){
-	        return true;
-	    }
+		if(solve2(maze, startX + xMove[i], startY + yMove[i], targetX, targetY)){
+			return true;
+		}
 	}
-	return false;
+    	return false;
     }
 
-    public static boolean printMaze2(char[][] maze, int startX, int StartY, int targetX, int targetY, ArrayList<Character> path){
-        if(startX < 0 || startX >= maze.length || startY < 0 || startY >= maze[0].length || maze[startX][startY] == 'X' || visited[startx][startY]){
+    public static boolean solve3(char[][] maze, int startX, int startY, int targetX, int targetY, List<Character> path){
+    	if(startX == targetX && startY == targetY){
+		return true;
+	}
+	
+	if(startX >= maze.length || startY >= maze[0].length || startX < 0 || startY < 0 || maze[startX][startY] == 'X'){
 		return false;
 	}
-
-	if(startX == targetX && startY == targetY){
-	    System.out.println(path);
-	    return true;
-	}
-
+	
 	maze[startX][startY] = 'X';
-
-	int[] dx = {1, 0, -1, 0};
-	int[] dy = {0, 1, 0, -1};
-        char[] direction = {'D', 'R', 'U', 'L'};
+	int[] xMove = {1, 0, -1, 0};
+	int[] yMove = {0, 1, 0, -1};
+	char[] dir = {'D', 'R', 'U', 'L'};
 
 	for(int i = 0; i < 4; i++){
-	    path.add(direction[i]);
-	    if(printMaze2(maze, startX + dx[i], startY + dy[i], targetX, targetY, path)){
-	        return true;
-	    }
-	    path.remove(path.size() - 1);
+		path.add(dir[i]);
+		if(solve3(maze, startX + xMove[i], startY + yMove[i], targetX, targetY, path)){
+			return true;
+		}
+		path.remove(path.size() - 1);
 	}
 	return false;
     }
-
-
 
     
 }
