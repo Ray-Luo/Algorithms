2549d4b67e9aab94d950d31188152efe47444474
Ray-Luo <leiluo.ray@gmail.com>
2017-11-21 11:29:10 -0600

    finished Permutation

--- Permutation.java
+++ Permutation.java
@@ -1,33 +1,32 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Permutation{
 	public static void main(String[] args){
-
+		int[] num = {1, 2, 3};
+		List<List<Integer>> result = new ArrayList<>();
+		solve1(result, num, new ArrayList<Integer>(), 0);
+		System.out.print(result.toString());
 	}
 
-	public static List<List<Integer>> permutation(int[] nums){
-		List<Integer> numList = new ArrayList<Integer>();
-		Array.sort(num);
-		for(int i = 0; i < nums.length; i++){
-			numList.add(num[i]);
+	public static void solve1(List<List<Integer>> result, int[] num, List<Integer> item, int index){
+		if(item.size() == num.length){
+			result.add(new ArrayList<>(item));
+			return;
 		}
-		return permute(new ArrayList<Integer>(), numList);
+
+		for(int i = 0; i < num.length; i++){
+			if(item.contains(num[i])){
+				continue;
+			}else{
+				item.add(num[i]);
+				solve1(result, num, item, i + 1);
+				item.remove(item.size() - 1);
+			}
+
+		}
+		return;
 	}
 
-	public List<List<Integer>> permute(List<Integer> cur, List<Integer> num){
-		List<List<Integer>> results = new ArrayList<ArrayList<Integer>>();
-
-		if(num.size() == 0){
-			results.add(cur);
-			return results;
-		}
-
-		for(int i = 0; i < num.size(); i++){
-			ArrayList<Integer> newCur = new ArrayList<Integer>(cur);
-			newCur.add(num.get(i));
-			ArrayList<Integer> newNum = new ArrayList<Integer>(num);
-			newNum.remove(i);
-			results.addAll(permute(newCur, newNum));
-		}
-		return results;
-	}
 }
 
